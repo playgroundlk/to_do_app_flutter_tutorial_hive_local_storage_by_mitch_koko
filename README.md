94dcefbe4054ae75702a1449e863662cfc509324
thilokaviraj <thilokaviraj@gmail.com>
2024-09-30 11:04:04 +0530

    Create Flutter project

--- README.md
+++
@@ -1,2 +0,0 @@
-# to_do_app_flutter_tutorial_hive_local_storage_by_mitch_koko
-A fully functional TODO app made with Flutter! I used Hive for the local storage back-end.
---
+++ android/app/src/main/kotlin/com/example/to_do_app_flutter_tutorial_hive_local_storage_by_mitch_koko/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.to_do_app_flutter_tutorial_hive_local_storage_by_mitch_koko
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
